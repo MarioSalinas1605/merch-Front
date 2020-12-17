f432c7df2ca07783704052f03089afc6b8f5c6dc
Mario Salinas <mario_1605@live.com.mx>
2020-12-17 07:12:13 -0600

    feat: integration of Google Maps

---
+++ src/components/Map.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const Map = () => {
+    const mapStyles = {
+        height: "50vh",
+        width: "100%"
+    }
+    const defaultCenter = {
+        lat: 19.4267261,
+        lng: -99.1718796
+    }
+    return (
+      <LoadScript googleMapsApiKey='AIzaSyAat86BHmy4MrJAHwu6itazLZQ1hb4Wdps'>
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={9}
+          center={defaultCenter}
+        >
+          <Marker position={defaultCenter} />
+        </GoogleMap>
+      </LoadScript>
+      )
+}
+
+export default Map;
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map'
 import '../styles/components/Success.css'
 
-const Success = () => (
-  <div className="Success">
-    <div className="Success-content">
-      <h2>Gracias por tu compra</h2>
-      <span>Tu pedido llegará en 3 días a tu dirección:</span>
-      <div className="Success-map">
-        Google Maps
+const Success = () => {
+  const { state } = useContext(AppContext);
+  const { buyer } = state;
+
+
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
+        <span>Tu pedido llegará en 3 días a tu dirección:</span>
+        <div className="Success-map">
+          <Map />
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Success;
